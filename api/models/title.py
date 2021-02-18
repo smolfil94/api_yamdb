ae689df11fe6bb0cd1a199f7ccaa1589ec8ca971
Alexander <ya.maskalev@yandex.ru>
2021-02-19 00:16:52 +0500

    Add Title model

---
+++ api/models/title.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from .category import Category
+from .genre import Genre
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=300)
+    year = models.IntegerField(null=True, blank=True)
+    description = models.CharField(max_length=1000, blank=True)
+    rating = models.IntegerField(null=True, blank=True)
+    genre = models.ManyToManyField(Genre,)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
