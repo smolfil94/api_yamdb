1c2107fdd8e8d19075a87552a243b1209ce079c3
Alexander <ya.maskalev@yandex.ru>
2021-02-18 22:25:06 +0500

    Add pagination

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -127,5 +127,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': (
+        'rest_framework.pagination.LimitOffsetPagination',
+    ),
+    'PAGE_SIZE': 10
 }
