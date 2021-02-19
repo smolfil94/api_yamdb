bbc2242bd8f1257b672df720bdfd148fd3ceffbb
maskalev <ya.maskalev@yandex.ru>
2021-02-19 10:29:12 +0500

    Add APIGenreViewSet

--- api/views/apigenreviewset.py
+++ api/views/apigenreviewset.py
@@ -1,2 +1,9 @@
-class APIGenreViewSet:
+from .createreadanddeleteviewset import CreateReadAndDeleteModelViewSet
+from ..models import Genre
+from ..serializers.genreserializer import GenreSerializer
+
+
+class APIGenreViewSet(CreateReadAndDeleteModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
     ...
