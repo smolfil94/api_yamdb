9f53c3507564c3d5e939ed28a2f1805d395bb2f9
Filipenkov Nikita <nikismol94@yandex.ru>
2021-02-20 16:57:47 +0300

    add permissions and serializers. Deleted managers

--- users/models.py
+++ users/models.py
@@ -2,35 +2,27 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
-from .managers import CustomUserManager
 
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        USER = 'user', _('User')
+        MODERATOR = 'moderator', _('Moderator')
+        ADMIN = 'admin', _('Admin')
 
-
-
-class CustomUser(AbstractUser):
-    class Roles(models.TextChoices):
-        ADMIN = 'admin', _('admin')
-        MODERATOR = 'moderator', _('moderator')
-        USER = 'user', _('user')
-
-    role = models.CharField(max_length=100, choices=Roles.choices, default=Roles.USER)
-    bio = models.TextField(blank=True)
-
-    confirmation_code = models.CharField(_('confirmation_code'), max_length=10, null=True, blank=True)
-
-    email = models.EmailField(_('email address'), unique=True, blank=False)
-
-    username = models.CharField(
-        max_length=150,
-        unique=True,
+    password = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(unique=True, blank=False)
+    bio = models.TextField(max_length=256, blank=True)
+    username = models.CharField(max_length=50, unique=True)
+    role = models.CharField(
+        max_length=10,
+        choices=Role.choices,
+        default=Role.USER
     )
-
-    objects = CustomUserManager()
-
-    @property
-    def is_moderator(self):
-        return self.role == self.Roles.MODERATOR
 
     @property
     def is_admin(self):
-        return self.role == self.Roles.ADMIN
+        return self.role == self.Role.ADMIN or self.is_superuser
+
+    @property
+    def is_moderator(self):
+        return self.role == self.Role.MODERATOR
