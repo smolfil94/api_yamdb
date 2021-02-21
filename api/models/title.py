786a3454a40508b31ee312737dcb149aa4b88322
maskalev <ya.maskalev@yandex.ru>
2021-02-21 11:19:40 +0500

    Fix test_02_title
    Add TitleFilter

--- api/models/title.py
+++ api/models/title.py
@@ -9,5 +9,5 @@ class Title(models.Model):
     year = models.IntegerField(null=True, blank=True)
     description = models.CharField(max_length=1000, blank=True)
     rating = models.IntegerField(null=True, blank=True)
-    genre = models.ManyToManyField(Genre,)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    genre = models.ManyToManyField(Genre, related_name='titles',)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name='titles',)
